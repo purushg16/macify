494b6315f83f665fe8a8bd1c461801c8b93add31
Purushothaman G <officialpurushothaman@gmail.com>
2024-02-21 21:50:44 +0530

    dropdown added in name & rental page in addProperty

--- src/components/Admin/pages/AddProperty/NamePage.tsx
+++ src/components/Admin/pages/AddProperty/NamePage.tsx
@@ -1,6 +1,17 @@
-import { Box, Button, Flex, Image, Input, VStack } from "@chakra-ui/react";
-import AddTitle from "../../elements/AddTitle";
+import {
+  Box,
+  Button,
+  Flex,
+  Icon,
+  Image,
+  Input,
+  VStack,
+} from "@chakra-ui/react";
 import building from "../../../../assets/app/building.png";
+import AddTitle from "../../elements/AddTitle";
+
+import { Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+import { TbChevronsDown } from "react-icons/tb";
 
 interface NamePageProps {
   forward?: () => void;
@@ -18,7 +29,32 @@ const NamePage = ({ forward }: NamePageProps) => {
 
       <VStack gap={4}>
         <Input bg="gray.50" placeholder="Property Name" />
-        <Input bg="gray.50" placeholder="Property Type" />
+
+        <Box position="relative">
+          <Input placeholder="Property Type" bg="gray.50" />
+          <Menu>
+            <MenuButton
+              _hover={{ bg: "gray.700" }}
+              as={Button}
+              pos="absolute"
+              right={2}
+              top={1}
+              p={0}
+              bg="black"
+              color="white"
+              size="sm"
+            >
+              <Icon as={TbChevronsDown} p={0} />
+            </MenuButton>
+            <MenuList p={1} borderRadius={20}>
+              <MenuItem>Download</MenuItem>
+              <MenuItem>Create a Copy</MenuItem>
+              <MenuItem>Mark as Draft</MenuItem>
+              <MenuItem>Delete</MenuItem>
+              <MenuItem>Attend a Workshop</MenuItem>
+            </MenuList>
+          </Menu>
+        </Box>
       </VStack>
 
       <Box>
