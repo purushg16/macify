f5e59fc7eb3733a18bb4ba3bc260315dee6875e7
Purushothaman G <officialpurushothaman@gmail.com>
2024-02-24 15:51:48 +0530

    room card redesigned

--- src/components/Admin/elements/AddProperty/RoomCard.tsx
+++ src/components/Admin/elements/AddProperty/RoomCard.tsx
@@ -9,7 +9,7 @@ import {
 import { BsPeopleFill } from "react-icons/bs";
 import { useLongPress } from "@uidotdev/usehooks";
 import RoomDetailsEditModal from "./RoomDetailsEditModal";
-import { TbBed } from "react-icons/tb";
+// import { TbBed } from "react-icons/tb";
 import Room from "../../../entities/room";
 
 const RoomCard = ({ room }: { room: Room }) => {
@@ -19,31 +19,54 @@ const RoomCard = ({ room }: { room: Room }) => {
   return (
     <Box
       {...attrs}
-      borderRadius="20px 20px 0px 0px"
+      bg="primary.50"
+      borderRadius={20}
+      aspectRatio="1/1"
       m="auto"
-      w="100%"
-      h={140}
-      bg="primary.100"
-      pos="relative"
       display="flex"
-      justifyContent="end"
-      alignItems="center"
-      border="5px solid"
-      borderColor="gray.100"
-      borderBottom="none"
+      p={2}
+      flexDirection="column"
+      justifyContent="start"
+      alignItems="start"
+      gap={4}
+      // boxShadow="inset 1px 4px 9px -1px #00000057"
+      boxShadow="rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;"
+      pos="relative"
     >
-      <Text pos="absolute" top={1} left={3} color="white">
+      <Text
+        bg="gray.50"
+        borderRadius={99}
+        px={4}
+        py={1}
+        boxShadow="rgba(0, 0, 0, 0.1) 0px 20px 25px -5px, rgba(0, 0, 0, 0.04) 0px 10px 10px -5px;"
+      >
         {room.roomName}
       </Text>
       <VStack>
-        <HStack bg="white" borderRadius={99} px={2} mr={2}>
-          <Text fontSize="lg"> {room.capacity} </Text>
-          <Icon as={TbBed} boxSize={4} />
-        </HStack>
-        <HStack bg="white" borderRadius={99} px={2} mr={2}>
+        <Box
+          bg="gray.50"
+          pos="absolute"
+          bottom={2}
+          right={2}
+          borderRadius={20}
+          px={2}
+          aspectRatio="1/1"
+          w="40%"
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          boxShadow="rgba(0, 0, 0, 0.1) 0px 20px 25px -5px, rgba(0, 0, 0, 0.04) 0px 10px 10px -5px;"
+        >
+          <HStack alignItems="baseline">
+            <Text fontSize="2xl">{room.capacity}</Text>
+            <Icon as={BsPeopleFill} boxSize={4} />
+          </HStack>
+        </Box>
+
+        {/* <HStack bg="white" borderRadius={99} px={2} mr={2}>
           <Text fontSize="lg"> {room.capacity} </Text>
           <Icon as={BsPeopleFill} boxSize={4} />
-        </HStack>
+        </HStack> */}
       </VStack>
       <RoomDetailsEditModal isOpen={isOpen} onClose={onClose} room={room} />
     </Box>
--- src/components/Admin/elements/AddProperty/RoomCardGrid.tsx
+++ src/components/Admin/elements/AddProperty/RoomCardGrid.tsx
@@ -8,12 +8,12 @@ const RoomCardGrid = () => {
   return (
     <SimpleGrid
       mx="auto"
-      w={{ base: "90%", md: "80%", lg: "70%" }}
-      columns={{ base: 2, md: 3, lg: 3 }}
+      w={{ base: "100%", md: "85%", lg: "90%" }}
+      columns={{ base: 2, md: 3, lg: 4 }}
       spacingX={4}
       spacingY={4}
       bg="#f6f6f6"
-      p={{ base: 6, md: 3, lg: 5 }}
+      p={{ base: 4, md: 3, lg: 5 }}
       borderRadius={20}
       maxH={350}
       overflowY="auto"
