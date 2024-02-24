0dc61715079acd768cc68877368931bdc7e19bd2
Purushothaman G <officialpurushothaman@gmail.com>
2024-02-24 12:21:24 +0530

    ScrollRestoration added in addproperty page

--- src/components/Admin/pages/AddProperty/AddPropertyPage.tsx
+++ src/components/Admin/pages/AddProperty/AddPropertyPage.tsx
@@ -1,10 +1,12 @@
 import { Flex } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
+import { ScrollRestoration } from "react-router-dom";
 
 const AddPropertyPage = () => {
   return (
     <Flex flexDir="column" gap={8} alignItems="center">
       <Outlet />
+      <ScrollRestoration />
     </Flex>
   );
 };
