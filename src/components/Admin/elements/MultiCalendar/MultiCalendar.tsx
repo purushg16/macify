261118a61f205851d61c51473f229d2eb9956a14
Purushothaman G <officialpurushothaman@gmail.com>
2024-02-27 18:12:15 +0530

    multicalendar practice

--- src/components/Admin/elements/MultiCalendar/MultiCalendar.tsx
+++ src/components/Admin/elements/MultiCalendar/MultiCalendar.tsx
@@ -1,16 +1,50 @@
 import FullCalendar from "@fullcalendar/react";
-import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
+import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import { DateSelectArg, EventContentArg } from "@fullcalendar/core/index.js";
+import { Box } from "@chakra-ui/react";
 
-export default function MultiCalendar() {
+const MyCalendar = () => {
+  const handleDateSelect = (selectInfo: DateSelectArg) => {
+    // Handle date selection logic
+    console.log(selectInfo);
+  };
+
+  const handleEventContent = (eventContent: EventContentArg) => {
+    const checkinTime = new Date(eventContent.event.start!);
+    const checkoutTime = new Date(eventContent.event.end!);
+
+    return (
+      <Box>
+        <div
+        //   style={{
+        //     color: "white",
+        //     borderRadius: "3px",
+        //     padding: "2px",
+        //     marginBottom: "2px",
+        //   }}
+        >
+          {`${checkinTime.toLocaleTimeString()} - ${checkoutTime.toLocaleTimeString()}`}
+        </div>
+      </Box>
+    );
+  };
+
   return (
     <FullCalendar
-      plugins={[dayGridPlugin]}
+      plugins={[dayGridPlugin, interactionPlugin]}
       initialView="dayGridMonth"
-      weekends={false}
       events={[
-        { title: "event 1", date: "2024-02-01" },
-        { title: "event 2", date: "2024-02-02" },
+        {
+          title: "Event 1",
+          start: "2024-02-04T20:00:00",
+          end: "2024-02-05T02:00:00",
+        },
       ]}
+      select={handleDateSelect}
+      eventContent={handleEventContent}
     />
   );
-}
+};
+
+export default MyCalendar;
