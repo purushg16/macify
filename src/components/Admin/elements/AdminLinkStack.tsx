305a599cfc888a65a04e930b251fad4b9a77ef23
Purushothaman G <officialpurushothaman@gmail.com>
2024-02-27 18:23:05 +0530

    role based setup in admin link stack

--- src/components/Admin/elements/AdminLinkStack.tsx
+++ src/components/Admin/elements/AdminLinkStack.tsx
@@ -1,9 +1,13 @@
 import { Box, VStack } from "@chakra-ui/react";
 import { LiteNavigationButton } from "./Button";
 import { useLocation } from "react-router-dom";
+import useRoleStore from "../../store/roleStore";
 
 const AdminLinkStack = () => {
   const location = useLocation().pathname.split("/")[2];
+  const role = useRoleStore((s) => s.role);
+  // const setRole = useRoleStore((s) => s.setRole);
+  // setRole("Manager");
 
   return (
     <Box px={{ base: 4, md: 8, lg: 16 }}>
@@ -18,16 +22,29 @@ const AdminLinkStack = () => {
           link="/admin/calendar"
           isActive={location === "calendar"}
         />
-        <LiteNavigationButton
-          text="Properties"
-          link="/admin/properties"
-          isActive={location === "properties"}
-        />
-        <LiteNavigationButton
-          text="Manager"
-          link="/admin/manager"
-          isActive={location === "manager"}
-        />
+
+        {role === "Admin" && (
+          <>
+            <LiteNavigationButton
+              text="Manager"
+              link="/admin/manager"
+              isActive={location === "manager"}
+            />
+            <LiteNavigationButton
+              text="Properties"
+              link="/admin/properties"
+              isActive={location === "properties"}
+            />
+          </>
+        )}
+
+        {role === "Manager" && (
+          <LiteNavigationButton
+            text="Account"
+            link="/manager/account"
+            isActive={location === "properties"}
+          />
+        )}
       </VStack>
     </Box>
   );
