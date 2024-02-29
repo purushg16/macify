10fa1cb6304790fcba99b653234354e65754bd69
Purushothaman G <officialpurushothaman@gmail.com>
2024-02-29 13:44:55 +0530

    UI- login page

--- src/components/Admin/pages/Authentication/LoginPage.tsx
+++ src/components/Admin/pages/Authentication/LoginPage.tsx
@@ -1,13 +1,90 @@
-import { Box } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Heading,
+  Icon,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import Title from "../../elements/Title";
+import { Link } from "react-router-dom";
+import { BsEye, BsEyeSlash } from "react-icons/bs";
+import { useState } from "react";
 
 const LoginPage = () => {
+  const [show, setShow] = useState(false);
+  const handleClick = () => setShow(!show);
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <Box>
-      <Title
-        heading="Welcome Back, Captain"
-        subtitle="Login using your credentials"
-      />
+    <Box
+      textAlign="center"
+      border="1px solid"
+      mx={4}
+      px={12}
+      py={8}
+      borderRadius={20}
+      borderColor="gray.50"
+    >
+      <Heading mb={8} fontSize="3xl">
+        Macify
+      </Heading>
+      <VStack my={4}>
+        <InputGroup>
+          <Input
+            placeholder="Username"
+            bg="gray.50"
+            mb={4}
+            value={username || ""}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <InputRightElement top={0}>
+            <Text>@</Text>
+          </InputRightElement>
+        </InputGroup>
+
+        <InputGroup>
+          <Input
+            placeholder="Password"
+            bg="gray.50"
+            type={show ? "text" : "password"}
+            pr="4.5rem"
+            value={password || ""}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <InputRightElement top={0}>
+            <Button onClick={handleClick} bg="none" _hover={{ bg: "none" }}>
+              <Icon as={show ? BsEyeSlash : BsEye} />
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </VStack>
+
+      <Text
+        mb={8}
+        textAlign="end"
+        fontSize="sm"
+        color="primary.500"
+        textDecor="underline"
+      >
+        <Link to="/auth/forgotPassword"> Forgot Password? </Link>
+      </Text>
+
+      <Title heading="Welcome Back!" subtitle="Login using your credentials" />
+
+      <Button
+        colorScheme="primary"
+        my={4}
+        px={8}
+        isDisabled={!username || !password}
+      >
+        Login
+      </Button>
     </Box>
   );
 };
