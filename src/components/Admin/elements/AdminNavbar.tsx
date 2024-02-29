974da3f1a159b868523f74f1630b375b502dedbf
Purushothaman G <officialpurushothaman@gmail.com>
2024-02-29 15:42:34 +0530

    notification button added in navbar

--- src/components/Admin/elements/AdminNavbar.tsx
+++ src/components/Admin/elements/AdminNavbar.tsx
@@ -1,7 +1,16 @@
-import { Box, Flex, Heading, Spacer } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  HStack,
+  Heading,
+  Icon,
+  IconButton,
+  Spacer,
+} from "@chakra-ui/react";
 import Btn from "../../Brand/elements/Button";
 import { Link } from "react-router-dom";
 import useRoleStore from "../../store/roleStore";
+import { BsBell } from "react-icons/bs";
 
 const AdminNavbar = () => {
   const role = useRoleStore((s) => s.role);
@@ -13,7 +22,16 @@ const AdminNavbar = () => {
           <Heading fontSize="3xl"> Macify </Heading>
         </Link>
         <Spacer />
-        <Btn text="D" primary />
+        <HStack>
+          <Link to="notifications">
+            <IconButton
+              bg="gray.50"
+              aria-label="notification"
+              icon={<Icon color="primary.500" as={BsBell} />}
+            />
+          </Link>
+          <Btn text="D" primary />
+        </HStack>
       </Flex>
     </Box>
   );
