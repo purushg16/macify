82b7c4fe059d5088428d78c4bc1e9400b95abea4
Purushothaman G <officialpurushothaman@gmail.com>
2024-02-29 22:12:45 +0530

    UI - Notification cards created

---
+++ src/components/Admin/elements/NotificationCard.tsx
@@ -0,0 +1,46 @@
+import {
+  Box,
+  Button,
+  Flex,
+  GridItem,
+  HStack,
+  Heading,
+  Icon,
+  Text,
+} from "@chakra-ui/react";
+import { BsClockFill, BsPeopleFill } from "react-icons/bs";
+
+const NotificationCard = () => {
+  return (
+    <Flex
+      justify="space-between"
+      p={4}
+      bg="primary.50"
+      borderRadius={10}
+      align="center"
+      cursor="pointer"
+      _hover={{ bg: "primary.100" }}
+      transition="all 0.7s"
+    >
+      <Box>
+        <Heading fontSize="xl"> Sunny's Villa </Heading>
+        <HStack mt={2} gap={4}>
+          <GridItem>
+            <Text display="flex" gap={2} alignItems="center">
+              <Icon as={BsPeopleFill} /> 3
+            </Text>
+          </GridItem>
+          <GridItem> | </GridItem>
+          <GridItem>
+            <Text display="flex" gap={2} alignItems="center">
+              <Icon as={BsClockFill} /> Feb 29, 2024
+            </Text>
+          </GridItem>
+        </HStack>
+      </Box>
+      <Button> Decline </Button>
+    </Flex>
+  );
+};
+
+export default NotificationCard;
--- src/components/Admin/pages/AdminNotificationsPage.tsx
+++ src/components/Admin/pages/AdminNotificationsPage.tsx
@@ -1,5 +1,29 @@
+import { Box, SimpleGrid } from "@chakra-ui/react";
+import Title from "../elements/Title";
+import NotificationCard from "../elements/NotificationCard";
+
 const AdminNotificationsPage = () => {
-  return <div>AdminNotifications</div>;
+  return (
+    <Box>
+      <Title
+        heading="Notifications"
+        subtitle="See all the bookings here"
+        align="left"
+      />
+
+      <Box bg="#f5f5f5" p={4} py={6} borderRadius={10} mt={4}>
+        <SimpleGrid columns={1} spacing={4}>
+          <NotificationCard />
+          <NotificationCard />
+          <NotificationCard />
+          <NotificationCard />
+          <NotificationCard />
+          <NotificationCard />
+          <NotificationCard />
+        </SimpleGrid>
+      </Box>
+    </Box>
+  );
 };
 
 export default AdminNotificationsPage;
