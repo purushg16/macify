d0bd7f564f0f78f389a6067610e2eca0aeece463
Purushothaman G <officialpurushothaman@gmail.com>
2024-02-29 15:37:28 +0530

    UI-forgot password input added hint added

--- src/components/Admin/pages/Authentication/ForgotPasswordPage.tsx
+++ src/components/Admin/pages/Authentication/ForgotPasswordPage.tsx
@@ -1,30 +1,76 @@
 import {
   Button,
+  GridItem,
+  HStack,
   Input,
   InputGroup,
   InputRightElement,
   Text,
+  VStack,
 } from "@chakra-ui/react";
 import Title from "../../elements/Title";
+import { useState } from "react";
+import AnimateMove from "../../../motions/Move";
 
 const ForgotPasswordPage = () => {
+  const [password, setPassword] = useState("");
+  const [cPassword, setCPassword] = useState("");
+
   return (
     <>
-      <InputGroup mb={8}>
-        <Input placeholder="Username" bg="gray.50" />
-        <InputRightElement top={0}>
-          <Text>@</Text>
-        </InputRightElement>
-      </InputGroup>
+      {!!password && !!cPassword && password !== cPassword && (
+        <AnimateMove>
+          <Text color="primary.500" fontSize="sm">
+            Password does not match!
+          </Text>
+        </AnimateMove>
+      )}
+      <VStack mb={8}>
+        <InputGroup>
+          <Input
+            placeholder="Password"
+            bg="gray.50"
+            type="password"
+            value={password || ""}
+            onChange={(e) => setPassword(e.target.value!)}
+          />
+          <InputRightElement top={0}>
+            <Text>#</Text>
+          </InputRightElement>
+        </InputGroup>
+
+        <InputGroup mt={4}>
+          <Input
+            placeholder="Confirm Password"
+            bg="gray.50"
+            value={cPassword || ""}
+            onChange={(e) => setCPassword(e.target.value!)}
+          />
+          <InputRightElement top={0}>
+            <Text>#</Text>
+          </InputRightElement>
+        </InputGroup>
+      </VStack>
 
       <Title
         heading="Reset Password"
-        subtitle="Enter your email & reset password here"
+        subtitle="Enter your new password & click 'Reset'"
       />
 
-      <Button colorScheme="primary" mt={4} mb={8} px={8}>
-        Confirm
-      </Button>
+      <HStack mt={8} mb={4}>
+        <GridItem>
+          <Button px={8}>Cancel</Button>
+        </GridItem>
+        <GridItem>
+          <Button
+            colorScheme="primary"
+            px={8}
+            isDisabled={!(!!password && !!cPassword && password === cPassword)}
+          >
+            Reset
+          </Button>
+        </GridItem>
+      </HStack>
     </>
   );
 };
