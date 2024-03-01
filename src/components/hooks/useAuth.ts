57b26a2041135ef02c42faf73fb2289a712e0456
Purushothaman G <officialpurushothaman@gmail.com>
2024-03-01 22:38:18 +0530

    error toast fired with authentications

--- src/components/hooks/useAuth.ts
+++ src/components/hooks/useAuth.ts
@@ -1,9 +1,13 @@
 import { useMutation } from "@tanstack/react-query";
 import { emailVerification, login, register } from "../api/auth-client";
 import { useNavigate } from "react-router-dom";
+import { useToast } from "@chakra-ui/react";
+import { AxiosError } from "axios";
+import { APIError } from "../entities/Error";
 
 const useRegister = () => {
   const navigate = useNavigate();
+  const toast = useToast();
 
   return useMutation({
     mutationFn: register.register,
@@ -13,29 +17,47 @@ const useRegister = () => {
       navigate("/auth/verifyEmail");
     },
 
-    onError: (err) => {
-      console.log(err);
-    },
+    onError: (err: AxiosError<APIError>) =>
+      toast({
+        title: err.response?.data?.error,
+        status: "error",
+        position: "top",
+        duration: 3000,
+      }),
   });
 };
 
 const useVerifyEmail = () => {
   const navigate = useNavigate();
+  const toast = useToast();
 
   return useMutation({
     mutationFn: emailVerification.verifyEmail,
     onSuccess: () => navigate("/admin"),
-    onError: (err) => console.log(err),
+    onError: (err: AxiosError<APIError>) =>
+      toast({
+        title: err.response?.data?.error,
+        status: "error",
+        position: "top",
+        duration: 3000,
+      }),
   });
 };
 
 const useLogin = () => {
   const navigate = useNavigate();
+  const toast = useToast();
 
   return useMutation({
     mutationFn: login.login,
     onSuccess: () => navigate("/admin"),
-    onError: (err) => console.log(err),
+    onError: (err: AxiosError<APIError>) =>
+      toast({
+        title: err.response?.data?.error,
+        status: "error",
+        position: "top",
+        duration: 3000,
+      }),
   });
 };
 
