ba077e8cbbf7b4b7c867d49fa4d95bc48fe28b70
Purushothaman G <officialpurushothaman@gmail.com>
2024-03-03 08:41:36 +0530

    properties sample data added

---
+++ src/components/data/properties.ts
@@ -0,0 +1,68 @@
+export default [
+  {
+    _id: "65d77f3f441ad240998b4b7b",
+    propertyName: "Luxury hostel",
+    propertyType: "hostel",
+    rooms: [
+      {
+        roomName: "Master Suite",
+        guestCapacity: 2,
+        beds: [
+          {
+            bedNo: 1,
+            _id: "65d77f3f441ad240998b4b7d",
+          },
+          {
+            bedNo: 2,
+            _id: "65d77f3f441ad240998b4b7e",
+          },
+        ],
+        _id: "65d77f3f441ad240998b4b7c",
+      },
+      {
+        roomName: "Guest Room",
+        guestCapacity: 3,
+        beds: [
+          {
+            bedNo: 1,
+            _id: "65d77f3f441ad240998b4b80",
+          },
+          {
+            bedNo: 2,
+            _id: "65d77f3f441ad240998b4b81",
+          },
+          {
+            bedNo: 3,
+            _id: "65d77f3f441ad240998b4b82",
+          },
+        ],
+        _id: "65d77f3f441ad240998b4b7f",
+      },
+    ],
+  },
+  {
+    _id: "65d958be2a773c387290a00e",
+    propertyName: "Sunnys hotel",
+    propertyType: "hotel",
+    rooms: [
+      {
+        roomName: "room 101",
+        guestCapacity: 2,
+        _id: "65d958be2a773c387290a00f",
+        beds: [],
+      },
+      {
+        roomName: "room 102",
+        guestCapacity: 3,
+        _id: "65d958be2a773c387290a010",
+        beds: [],
+      },
+    ],
+  },
+  {
+    _id: "65d958df2a773c387290a013",
+    propertyName: "nandhi villa",
+    propertyType: "villa",
+    rooms: [],
+  },
+];
