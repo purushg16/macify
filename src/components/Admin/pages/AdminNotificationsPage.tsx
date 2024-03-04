30cf4e7378d3c78639c3ea2bfa4d53b0c49e352b
Purushothaman G <officialpurushothaman@gmail.com>
2024-03-04 19:44:46 +0530

    notifications page connected with API data

--- src/components/Admin/pages/AdminNotificationsPage.tsx
+++ src/components/Admin/pages/AdminNotificationsPage.tsx
@@ -1,9 +1,12 @@
-import { Box, SimpleGrid } from "@chakra-ui/react";
+import { Box, SimpleGrid, Spinner } from "@chakra-ui/react";
 import Title from "../elements/Title";
 import NotificationCard from "../elements/NotificationCard";
-import bookingsToApprove from "../../data/bookingsToApprove";
+import { useGetBookingsToApprove } from "../../hooks/useAdmin";
 
 const AdminNotificationsPage = () => {
+  const { data, isLoading } = useGetBookingsToApprove();
+
+  if (isLoading) return <Spinner />;
   return (
     <Box>
       <Title
@@ -14,7 +17,7 @@ const AdminNotificationsPage = () => {
 
       <Box mt={4}>
         <SimpleGrid columns={1} spacing={4}>
-          {bookingsToApprove.map((booking) => (
+          {data?.data.map((booking) => (
             <NotificationCard key={booking._id} booking={booking} />
           ))}
         </SimpleGrid>
