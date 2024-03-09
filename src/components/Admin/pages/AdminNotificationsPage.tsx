534fc9d379b867055e3fe8283845f66868476e39
Purushothaman G <officialpurushothaman@gmail.com>
2024-03-09 14:29:37 +0530

    fetch req re-added in notification page

--- src/components/Admin/pages/AdminNotificationsPage.tsx
+++ src/components/Admin/pages/AdminNotificationsPage.tsx
@@ -1,13 +1,11 @@
 import { Box, SimpleGrid, Spinner } from "@chakra-ui/react";
 import Title from "../elements/Title";
 import NotificationCard from "../elements/NotificationCard";
-// import { useGetBookingsToApprove } from "../../hooks/useAdmin";
+import { useGetBookingsToApprove } from "../../hooks/useAdmin";
 import AnimateMove from "../../motions/Move";
-import data from "../../data/notifications";
 
 const AdminNotificationsPage = () => {
-  // const { data, isLoading } = useGetBookingsToApprove();
-  const isLoading = false;
+  const { data, isLoading } = useGetBookingsToApprove();
 
   return (
     <Box>
@@ -22,7 +20,7 @@ const AdminNotificationsPage = () => {
       ) : (
         <Box mt={4}>
           <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={8} p={2}>
-            {data.map((booking, index) => (
+            {data?.data.map((booking, index) => (
               <AnimateMove key={booking._id} delay={0.2 * (index + 1)}>
                 <NotificationCard booking={booking} />
               </AnimateMove>
