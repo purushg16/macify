73abafbab031b5033eafbb834423bd64953a134d
Purushothaman G <officialpurushothaman@gmail.com>
2024-03-09 22:01:45 +0530

    add property button redesigned

--- src/components/Admin/pages/PropertyPage.tsx
+++ src/components/Admin/pages/PropertyPage.tsx
@@ -1,36 +1,43 @@
-import { Button, Flex, SimpleGrid, Spinner } from "@chakra-ui/react";
-import { Link } from "react-router-dom";
-import Title from "../elements/Title";
-import PropertyCard from "../elements/PropertyCard";
-import AnimateMove from "../../motions/Move";
+import { Box, Flex, SimpleGrid, Spinner } from "@chakra-ui/react";
 import { useGetAllProperties } from "../../hooks/usePropertyServices";
+import AnimateMove from "../../motions/Move";
+import AddPropertyButton from "../elements/AddPropertyButton";
+import PropertyCard from "../elements/PropertyCard";
+import Title from "../elements/Title";
 
 const PropertyPage = () => {
   const { data: properties, isLoading } = useGetAllProperties();
 
   return (
     <Flex flexDir="column" gap={8}>
-      <Flex justify="space-between">
-        <Title
-          heading="Properties"
-          subtitle="Manage & see all the property's details"
-          align="left"
-        />
-        <Link to="/admin/properties/add/">
-          <Button> Add Property </Button>
-        </Link>
-      </Flex>
+      <Title
+        heading="Properties"
+        subtitle="Manage & see all the property's details"
+        align="left"
+      />
+
+      <Box borderBottom="1px solid" borderColor="gray.50">
+        <AddPropertyButton />
+      </Box>
 
       {isLoading ? (
         <Spinner />
       ) : (
-        <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={8} p={2}>
-          {properties?.data.map((property, i) => (
-            <AnimateMove delay={0.2 * (i + 1)}>
-              <PropertyCard property={property} />
-            </AnimateMove>
-          ))}
-        </SimpleGrid>
+        <Box>
+          <Title heading="All Properties" subtitle="" align="left" />
+          <SimpleGrid
+            columns={{ base: 1, md: 2, lg: 3 }}
+            spacing={8}
+            p={2}
+            mt={4}
+          >
+            {properties?.data.map((property, i) => (
+              <AnimateMove delay={0.2 * (i + 1)}>
+                <PropertyCard property={property} />
+              </AnimateMove>
+            ))}
+          </SimpleGrid>
+        </Box>
       )}
     </Flex>
   );
