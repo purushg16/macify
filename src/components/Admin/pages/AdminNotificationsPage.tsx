b3adced23e461f9cda02eb78d0b7c9eaa8b35c05
Purushothaman G <officialpurushothaman@gmail.com>
2024-03-15 09:48:51 +0530

    notifications replaced with original data

--- src/components/Admin/pages/AdminNotificationsPage.tsx
+++ src/components/Admin/pages/AdminNotificationsPage.tsx
@@ -4,18 +4,18 @@ import {
   Icon,
   SimpleGrid,
   VStack,
-  // Spinner
+  Spinner,
 } from "@chakra-ui/react";
 import Title from "../elements/Title";
 import NotificationCard from "../elements/NotificationCard";
-// import { useGetBookingsToApprove } from "../../hooks/useAdmin";
+import { useGetBookingsToApprove } from "../../hooks/useAdmin";
 import AnimateMove from "../../motions/Move";
-import notifications from "../../data/notifications";
+// import notifications from "../../data/notifications";
 import { Link } from "react-router-dom";
 import { BsFillPlusCircleFill } from "react-icons/bs";
 
 const AdminNotificationsPage = () => {
-  // const { data, isLoading } = useGetBookingsToApprove();
+  const { data, isLoading } = useGetBookingsToApprove();
 
   return (
     <Box>
@@ -38,19 +38,19 @@ const AdminNotificationsPage = () => {
         </VStack>
       </AnimateMove>
 
-      {/* {isLoading ? (
+      {isLoading ? (
         <Spinner />
-      ) : ( */}
-      <Box mt={4}>
-        <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={4}>
-          {notifications.map((booking, index) => (
-            <AnimateMove key={booking._id} delay={0.4 * (index + 1)}>
-              <NotificationCard booking={booking} />
-            </AnimateMove>
-          ))}
-        </SimpleGrid>
-      </Box>
-      {/* )} */}
+      ) : (
+        <Box mt={4}>
+          <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={4}>
+            {data?.data.map((booking, index) => (
+              <AnimateMove key={booking._id} delay={0.4 * (index + 1)}>
+                <NotificationCard booking={booking} />
+              </AnimateMove>
+            ))}
+          </SimpleGrid>
+        </Box>
+      )}
     </Box>
   );
 };
