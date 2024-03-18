b45ed1e2587ef6e379ce5e6d4670436ab2021a1d
Purushothaman G <officialpurushothaman@gmail.com>
2024-03-18 18:52:08 +0530

    success page redesign

--- src/components/Admin/pages/CustomerBooking/FinalPage.tsx
+++ src/components/Admin/pages/CustomerBooking/FinalPage.tsx
@@ -1,41 +1,25 @@
-import { Button, Box, Image } from "@chakra-ui/react";
-import AnimateMove from "../../../motions/Move";
+import { Button } from "@chakra-ui/react";
 import NavigatorWrapper from "../../elements/NavigatorWrapper";
-import Title from "../../elements/Title";
 import success from "../../../../assets/app/bookingSuccess.png";
+import BookingFooter from "../../elements/Booking/BookingFooter";
 
 const FinalPage = () => {
   return (
     <>
-      <AnimateMove delay={0.4}>
-        <Box pos="relative" mb={12}>
-          <Image src={success} alt="" w={250} pos="relative" zIndex={1} />
-          <Box
-            bg="white"
-            borderRadius="100%"
-            w={250}
-            aspectRatio="1/1"
-            pos="absolute"
-            top={25}
-            left={-2}
-          />
-        </Box>
-      </AnimateMove>
-
-      <AnimateMove delay={0.6}>
-        <Title
-          heading="Successfully Booked 🎉"
-          subtitle="Your booking has been confirmed"
-        />
-      </AnimateMove>
-
-      <AnimateMove delay={0.8}>
-        <NavigatorWrapper to="/booking">
-          <Button id="extra" colorScheme="primary">
-            Book Another!
-          </Button>
-        </NavigatorWrapper>
-      </AnimateMove>
+      <BookingFooter
+        title="Successfully Booked 🎉"
+        subheading="Your booking has been confirmed"
+        buttons={
+          <>
+            <NavigatorWrapper to="/booking">
+              <Button id="extra" colorScheme="primary">
+                Book Another!
+              </Button>
+            </NavigatorWrapper>
+          </>
+        }
+        image={success}
+      />
     </>
   );
 };
